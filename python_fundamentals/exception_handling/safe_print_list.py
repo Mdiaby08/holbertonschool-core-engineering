4eee756dcb4dc0a5646bfcd78c23fa06e27bdb3d
Mdiaby08 <diabymassire93@gmail.com>
2026-05-15 23:06:18 +0200

    Update safe_print_list.py

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
             count += 1
-    except Exception:
-        pass
+        except (ValueError, TypeError):
+            continue
     print()
     return count
