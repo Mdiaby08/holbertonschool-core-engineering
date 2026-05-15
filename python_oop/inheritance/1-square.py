b1a2fcdad5793d78c2d5523bbc7274af8138eed3
Mdiaby08 <diabymassire93@gmail.com>
2026-05-15 23:34:35 +0200

    Update and rename square.py to 1-square.py

---
+++ python_oop/inheritance/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defines a Square class that inherits from Rectangle."""
+Rectangle = __import__('0-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square using Rectangle."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
