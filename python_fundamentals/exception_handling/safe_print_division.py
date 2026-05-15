566497c3ec6947d18b0f85d05c8c2f9324b947ef
Mdiaby08 <diabymassire93@gmail.com>
2026-05-15 23:08:44 +0200

    Update safe_print_division.py

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
 def safe_print_division(a, b):
-    result = None
-    
     try:
         result = a / b
-    except:
+    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))
