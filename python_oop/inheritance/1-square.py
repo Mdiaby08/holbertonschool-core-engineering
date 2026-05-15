bf70c82daffb9cff541a060310dbe016b6836220
Mdiaby08 <diabymassire93@gmail.com>
2026-05-15 23:38:32 +0200

    Update 1-square.py

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
-"""Defines a Square class that inherits from Rectangle."""
-Rectangle = __import__('rectangle').Rectangle
+"""Module defining a Square class that inherits from Rectangle."""
+Rectangle = __import__('2-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square using Rectangle."""
+    """Square class, a specialized Rectangle where width equals height."""
 
     def __init__(self, size):
+        """Initialize a Square with the given size.
+
+        Args:
+            size: The size of the square (must be a positive integer).
+        """
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
