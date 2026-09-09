47d0198de410c74cce27485df604a657292e5a35
Massire <diabymassire93@gmail.com>
2026-09-09 11:59:01 +0200

    add a break

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -8,5 +8,8 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             continue
+        except IndexError:
+            break
     print()
     return count
+    
