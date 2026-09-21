54f171184f66b41c5e34eb14376ca61268909cb0
Massire <diabymassire93@gmail.com>
2026-09-21 12:50:20 +0200

    main modiefied

--- python_advanced/file_handling/main.py
+++ python_advanced/file_handling/main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
-read_file = __import__('read_file').read_file
 
+read_file = __import__('read_file').read_file
 read_file("my_file_0.txt")
+
+write_file = __import__('write_file').write_file
+
