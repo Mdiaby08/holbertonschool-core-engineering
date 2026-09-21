b0db0193b172a9b2b94b04aeab16399b87808031
Massire <diabymassire93@gmail.com>
2026-09-21 12:39:43 +0200

    add function to write files content

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""
+Write a function that writes a string to a text file (UTF-8) and returns the number of characters written:
+"""
 
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF-8) and returns the number of characters written"""
+    if filename == "":
+        return
 
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.write()(), end="")
