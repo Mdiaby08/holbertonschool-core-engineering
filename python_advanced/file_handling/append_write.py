4ab37bad9edda10dbb8fe1893b5f6680bd1c82e0
Mdiaby08 <diabymassire93@gmail.com>
2026-09-22 09:06:22 +0200

    Create append_write module for file handling
    Added a module for appending text to a file with UTF-8 encoding.

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+Module for appending text to a file.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string to the end of a text file (UTF-8)
+    and returns the number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
